00858011ed1c5a8c86e148b86cc0b46d24b53436
Laurent VANDELLE <vandellelaurent@yahoo.fr>
2019-05-25 12:15:38 +0200

    inital code commit

---
+++ arduino/AllumerLampe/AllumerLampe.ino
@@ -0,0 +1,12 @@
+
+const int LED_KITCHEN = 13;
+
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(LED_KITCHEN,OUTPUT)
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
